b13fc47cbf6bd5f401d0a694f494a53a890022c0
DengHaoyang97 <49272807+DengHaoyang97@users.noreply.github.com>
2023-11-17 07:21:17 +0800

    Update server.js

--- server.js
+++ server.js
@@ -15,10 +15,10 @@
 } */ //mongodb method(cnDB)//const kittySchema = require('./models/kitty');//schema requirement
 //
 //names
-const collectionName_user = 'users';  //login/create
-const uri = `mongodb+srv://userfornode:12345678900@book-managementsystem.cqntnli.mongodb.net/BookManage`;
+const collectionName_user = 'user';  //login/create
+const uri = `mongodb+srv://billydeng97:dhy97886886@cluster0.zsgzyzj.mongodb.net/381Project?retryWrites=true&w=majority`;
+const urih = `mongodb+srv://userfornode:12345678900@book-managementsystem.cqntnli.mongodb.net/BookManage`;
 //names
-
 //quotes
 const mongoose = require('mongoose');
 const express = require('express');
@@ -28,107 +28,115 @@ const app = express();
 const bodyParser=require('body-parser');
  app.use(bodyParser.urlencoded({ extended: true }));
 //quotes
-
+mongoose.connect(urih);	
 //shortcuts
 const closeDB=()=>mongoose.disconnect();	
-mongoose.connect(uri);	
+const openDB=()=>mongoose.connect(uri);	
 const db=mongoose.connection;
 //shortcuts
+
 app.use(session({
     userid: "session",  
-    keys: ["123"],
+    keys: ["123","bbb","666"],
 }))
-
-//bookSchema
+ 
+ //schemas
+ //books
  const bookSchema = new mongoose.Schema({
     title: String,
     author: String,
     year: Number
 });
 const Book = mongoose.model('books', bookSchema);
+//books/
 
 //functions
-   const handle_login = async (username, password) => {
+const matchNamePW = async (db,usn,psw) => {
+   try {let result = await db.collection(collectionName_user).findOne({ "username":`${usn}`,"password":`${psw}`});
+   return result !== null; }
+   catch (err) {
+   console.error('SomethingWrong', err);
+   return false;}} //match password and username
+
+const matchUserName = async (db,usn) => {
+   try {let result = await db.collection(collectionName_user).findOne({ "username":`${usn}`});
+      return result !== null; }
+   catch (err) {
+      console.error('SomethingWrong', err);
+      return false;}}  //function use to match username
+
+const createAcc=async (db,nusn,npsw)=>{
+	try{
+	let result=await db.collection(collectionName_user).insertOne({"username":`${nusn}`,"password":`${npsw}`});
+	return result !== 1;}
+	catch (err) {
+	      console.error('SomethingWrong', err);
+	      return false;}} //function use to create accounts
+    
+const handle_login = async (req,username,password) => {
    try{ 
+   await openDB();
    console.log("Connected DB");
-   let result= await db.collection(collectionName_user).findOne({ "username": username,"password":password});
+   let result=await matchNamePW(db,username,password);
    if(result){
-   console.log(result)
-   return result;
+   console.log("pw&usn matched");
+   let unique=await db.collection(collectionName_user).findOne({"username":`${username}`});
+   let Id=unique._id.toString();
+   console.log(Id);
+   req.session.dbid=Id;//cookie p2
+   return null;
    }
    else{
-   return null;
-   console.log("NOTCORRECT!!!")}
- 
-   
-  }
+   return {Message:"Wrong Username or Password"};
+   console.log("Not matched")}}
    catch (err){
-   console.error("Error while handle login",err);
-   }
+   console.error("Somthing Wrong!",err);}
    finally{
-   console.log("Disconnected DB");}}
+   await closeDB(); 
+   console.log("Disconnected DB");}} //login
  //  
    const handle_accCreate = async (username, password) => {
    try{  
+   await openDB();
    console.log("Connected DB");
-	let match = await db.collection(collectionName_user).findOne({"username":username});
-	if(match){
+        let match=await matchUserName(db,username);
+	if(match){let result=true;
 	console.log("Username used");
-	return true;
-	}
-	
+	return {Message:"User Name Already Used"};}
 	else{
-   let result=await db.collection(collectionName_user).insertOne({"username":username,"password":password});
-   console.log(username + " Created");
-   return false;}}
-   
-   
+   let result=await createAcc(db,username,password);
+       console.log("Wow Created new user welcome 666");
+       return null;}}
    catch (err){
-   console.error("Error while handle acccreate",err);
-   }
-   finally{
-   console.log("Disconnected DB");}}
-   
-   
-   
-   
+   console.error("SomthingWrong!",err);}
+   finally{await closeDB(); 
+   console.log("Disconnected DB");}}//acc create
+      
 //res req
-
 app.get('/login', (req, res) => {
    res.status(200).render('login.ejs',{Message:null});   
-  
-});
+  });
+
 app.post('/login', async(req,res) => {
-   const result=await handle_login(req.body.username,req.body.password);
-   if(result){
-   req.session.loggedIn = true;
-   req.session.authenticated = true;
-   req.session.userid = result._id.toString();
-   console.log("Logged as " + result.username);
-   res.redirect('/');
-	   }
-   else{res.status(200).render('login.ejs',{Message:"incorrect username or passwd"});}
-});
+   const result=await handle_login(req,req.body.username,req.body.password);
+   if(result && result.Message){res.status(200).render('login.ejs',{Message:result.Message});}
+   if(result==null){res.redirect('/');}});
 
 app.get('/createaccount', (req, res) => {
-   res.status(200).render('createaccount.ejs',{Message:null});   
-  });
-  
+   res.status(200).render('createaccount.ejs',{Message:null}); });
+ 
 app.post('/createaccount', async(req, res) => {
     const result=await handle_accCreate(req.body.username,req.body.password);
-    if(result){ res.status(200).render('createaccount.ejs',{Message:"User Name Already Used"});}
-	else{
-	res.status(200).render('login.ejs',{Message:"Successfully Create Account! Plz Login"});
-	}
+    if(result && result.Message){ res.status(200).render('createaccount.ejs',{Message:result.Message});}
   });
   
   app.get('/logout',(req,res)=>{
-  req.session.loggedIn = null;
+  req.session.id = null;
   res.redirect('/login');
-  })
-  
-  //books
-  
+  });
+
+//books
+
   app.get('/', async (req, res) => {
 	 //check if login
 	if (!req.session.loggedIn) {
@@ -200,5 +208,4 @@ app.post('/books/delete/:id', async (req, res) => {
 //end
 app.listen(process.env.PORT || 3000);
 
-   
 
